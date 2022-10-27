6d52a1c45d87830bf91d9d1d6fb6889c313ed4a4
teddywaweru <teddywaweru@gmail.com>
2022-10-27 08:35:06 +0300

    edit: remove esc for terminal

--- init.lua
+++ init.lua
@@ -23,7 +23,7 @@ return {
     ["<C-\\>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
   },
   t = {
-    ["<esc>"] = { "<C-\\><C-n>", desc = "Terminal normal mode" },
+    -- ["<esc>"] = { "<C-\\><C-n>", desc = "Terminal normal mode" },
     ["jk"] = { "<C-\\><C-n>", desc = "Terminal normal mode" },
   },
   },
