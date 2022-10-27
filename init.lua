280201968ab8d667f51c05f6bd4b33d15d71ca71
teddywaweru <teddywaweru@gmail.com>
2022-10-27 08:09:02 +0300

    init commit

--- init.lua
+++ init.lua
@@ -1,21 +1,55 @@
 
 return {
   -- require("plugin.options"),
+  updater = {
+    channel = "nightly",
+    auto_reload = false,
+    auto_quit = false,
+  },
   options = {
     opt = {
       wrap = true,
-      guifont = "Hack:h10"
+      guifont = "Hack:h10",
+      tabstop = 2,
+      softtabstop = 2,
+      expandtab = false
     },
     g = {
       neovide_refresh_rate = 50,
     }
   },
+  mappings = {
+     n = {
+    ["<C-\\>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+  },
+  t = {
+    ["<esc>"] = { "<C-\\><C-n>", desc = "Terminal normal mode" },
+    ["jk"] = { "<C-\\><C-n>", desc = "Terminal normal mode" },
+  },
+  },
   lsp = {
     skip_setup = { "rust_analyzer" }, -- skip lsp setup because rust-tools will do it itself
     skip_setup = { "tsserver" }, --skip lsp setup because rust-tools will do it itself
+    skip_setup = { "clangd" },
+    ["server-settings"] = {
+      clangd = {
+        capabilities = {
+          offsetEncoding = "utf-8",
+        },
+      },
+    },
   },
   plugins = {
     init = {
+      {
+        "p00f/clangd_extensions.nvim",
+        after = "mason-lspconfig.nvim",
+        config = function()
+          require("clangd_extensions").setup {
+            server = astronvim.lsp.server_settings "clangd"
+          }
+        end,
+      },
       {
         "simrat39/rust-tools.nvim",
         after = "mason-lspconfig.nvim", -- make sure to load after mason-lspconfig
@@ -38,7 +72,7 @@ return {
        -- {'CRAG666/code_runner.nvim'} --install code_runner
     },
     ["mason-lspconfig"] = {
-      ensure_installed = { "rust_analyzer", "tsserver" }, -- install rust_analyzer
+      ensure_installed = { "rust_analyzer", "tsserver", "clangd" }, -- install rust_analyzer
     },
   },
 }
