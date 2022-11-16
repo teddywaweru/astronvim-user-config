b1f68282d25460e17e93247d0b701be25e009b19
teddywaweru <teddywaweru@gmail.com>
2022-11-16 09:27:42 +0300

    add: leap plugin

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-
 return {
   -- require("plugin.options"),
   updater = {
@@ -67,9 +66,29 @@ return {
             server = astronvim.lsp.server_settings "tsserver"
             }
           end,
-      }
+      },
        -- {"folke/tokyonight.nvim"}, --install tokyonight
        -- {'CRAG666/code_runner.nvim'} --install code_runner
+      ["ggandor/leap-spooky.nvim"] = {
+        max_phase_one_targets = nil,
+        highlight_unlabeled_phase_one_targets = false,
+        max_highlighted_traversal_targets = 10,
+        case_sensitive = false,
+        equivalence_classes = { ' \t\r\n', },
+        substitute_chars = {},
+        safe_labels = { 's', 'f', 'n', 'u', 't' },
+        labels = { 's', 'f', 'n', 'j', 'k' },
+        special_keys = {
+          repeat_search = '<enter>',
+          next_phase_one_target = '<enter>',
+          next_target = {'<enter>', ';'},
+          prev_target = {'<tab>', ','},
+          next_group = '<space>',
+          prev_group = '<tab>',
+          multi_accept = '<enter>',
+          multi_revert = '<backspace>',
+        }
+      } --install leap
     },
     ["mason-lspconfig"] = {
       ensure_installed = { "rust_analyzer", "tsserver", "clangd" }, -- install rust_analyzer
